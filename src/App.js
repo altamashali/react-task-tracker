c7d1d39d01f60374c2b1ca0f378e0a4651ba472a
Altamash Ali <14144008+altamashali@users.noreply.github.com>
2021-05-20 21:17:38 -0500

    Tutorial First-Runthrough

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+import Footer from './components/Footer'
+import About from './components/About'
+import { useState } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+ 
+
+const App = () => {
+  const[showAddTask, setShowAddTask] = useState(false);
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at school',
+        day: 'Feb 6th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    }
+  ])
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000 + 1)
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+       task.id === id ? { ...task, reminder: !task.reminder} : task
+      )
+    )
+
+  }
+
+  return (
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+        
+        <Route path='/' exact render={(props) => (
+          <>
+            {showAddTask && <AddTask onAdd={addTask} />}
+            { tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show' }
+          </>
+        )} />
+        <Route path='/about' component={About}/>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
